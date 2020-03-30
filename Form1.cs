4a4c341b671331bc42d1dadade5ff5392e131b8e
CAHIH <55281316+CAHIH@users.noreply.github.com>
2020-03-30 14:46:47 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,40 @@ namespace MyProgram
             InitializeComponent();
         }
 
+        Dictionary<string, string> RusWords = new Dictionary<string, string>();
+        Dictionary<string, string> EngWords = new Dictionary<string, string>();
+        Dictionary<string, string> UkrWords = new Dictionary<string, string>();
         private void Form1_Load(object sender, EventArgs e)
         {
+            RusWords.Add("Новичок" , "Новичок");
+            RusWords.Add("Любитель" , "Любитель");
+            EngWords.Add("Новичок", "Newbie");
+            EngWords.Add("Любитель", "Amateur");
+            UkrWords.Add("Новичок", "Новачок(укр)");
+            UkrWords.Add("Любитель", "Любитель(укр)");
+
+        }
+     
+    private void pictureBox6_Click_1(object sender, EventArgs e)
+        {
+            button1.Text = EngWords["Новичок"];
+            button2.Text = EngWords["Любитель"];
+
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            button1.Text = RusWords["Новичок"];
+            button2.Text = RusWords["Любитель"];
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            button1.Text = UkrWords["Новачок(укр)"];
+            button2.Text = UkrWords["Любитель(укр)"];
 
         }
     }
+
 }
